27657b20ecb626f31824acfc4bfb5ecc63921275
botonger <shinecheyenne@gmail.com>
2022-08-25 09:39:09 +0900

    containerized

--- source/src/main/java/org/source/SourceApplication.java
+++ source/src/main/java/org/source/SourceApplication.java
@@ -1,5 +1,6 @@
 package org.source;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 public class SourceApplication {
 
+    @Value("${url}")
+    private String url;
+
     public static void main(String[] args) {
         SpringApplication.run(SourceApplication.class, args);
     }
@@ -15,7 +19,7 @@ public class SourceApplication {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:9001")
+                .baseUrl(url)
                 .build();
     }
 }
